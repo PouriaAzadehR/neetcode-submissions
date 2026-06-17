85da626bf6a26e2901905492b719a96bdc9462c5
Pouria Azadehranjbar <pouria_azadehranjbar@sfu.ca>
2026-06-16 18:42:09 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+
+
+    # strategy: brute force
+    # time complexity: o(k*n + n*n)
+    # space complexisty: o(k)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        k_most_frequent_numbers = list()
+        
+     
+    ################################ Frequency Counter ################################
+    
+        # key: number in the list | value: frequency
+        dict_frequency_numbers = dict()
+
+        # time complexity: o(n)
+        for number in nums:
+            dict_frequency_numbers.setdefault(number, 0)
+            dict_frequency_numbers[number] += 1
+
+        
+    ################################ Top K Selecting ################################
+
+        k_most_frequent = list()
+
+        # time complexity: o(k*n)
+        for k_index in range(k):
+            candidate_key, candidate_value = -1001, -1001
+
+            for key, value in dict_frequency_numbers.items():
+                if value > candidate_value:
+                    candidate_key, candidate_value = key, value
+
+            k_most_frequent.append(candidate_key)
+            dict_frequency_numbers.pop(candidate_key)
+
+        return k_most_frequent
+
+
